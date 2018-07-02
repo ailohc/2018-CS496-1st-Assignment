9dbb773da042d849a67a2abb04f913e3a3ac1f4b
Dain Lee <ailohc@naver.com>
2018-07-02 10:06:15 +0900

    third tab need to be fixed
    need fix

--- app/src/main/java/firstproject/cs496/merge/ThirdFragment.kt
+++ app/src/main/java/firstproject/cs496/merge/ThirdFragment.kt
@@ -1,5 +1,6 @@
 package firstproject.cs496.merge
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
@@ -10,8 +11,11 @@ import android.view.ViewGroup
 
 class ThirdFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater!!.inflate(R.layout.fragment_third, container, false)
-    }
+        var rootView = inflater.inflate(R.layout.fragment_third, container, false);
+        var context = rootView.getContext();
+        var intent = Intent(context, ThirdActivity::class.java)
+        startActivity(intent)
 
+        return rootView;
+    }
 }
