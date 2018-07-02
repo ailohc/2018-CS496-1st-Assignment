d87096cfd30fd36a31a84ea2052de5609ea462f0
Dain Lee <ailohc@naver.com>
2018-07-02 15:40:37 +0900

    error fix by delaying

--- app/src/main/java/firstproject/cs496/merge/SplashActivity.kt
+++ app/src/main/java/firstproject/cs496/merge/SplashActivity.kt
@@ -1,5 +1,6 @@
 package firstproject.cs496.merge
 
+import android.Manifest
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
@@ -8,15 +9,35 @@ import android.view.Window
 import android.os.Handler
 import android.content.Intent
 import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 
 class SplashActivity : AppCompatActivity() {
 
+    private var mDelayHandler: Handler? = null
+    private val SPLASH_DELAY: Long = 3000 //3 seconds
+
+    companion object {
+        val PERMISSIONS_REQUEST_READ_CONTACTS = 100
+    }
+
+    internal val mRunnable: Runnable = Runnable {
+
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(
+                        Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.READ_CONTACTS),
+                    SplashActivity.PERMISSIONS_REQUEST_READ_CONTACTS)
+            //callback onRequestPermissionsResult
+        }
+        mDelayHandler = Handler()
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
         // Start home activity
-        startActivity(Intent(this@SplashActivity, MainActivity::class.java))
-
-        // close splash activity
-        finish()
     }
 }
